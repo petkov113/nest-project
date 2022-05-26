4eb4c1935eb3e52c33c112c266742313ff8e6693
PP <pyotr.petkov@gmail.com>
2022-05-26 21:12:40 +0300

    format code all at once

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
-import { ConfigModule } from '@nestjs/config';
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config'
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
   controllers: [],
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const PORT = process.env.PORT || 4000;
-  const app = await NestFactory.create(AppModule);
+  const PORT = process.env.PORT || 4000
+  const app = await NestFactory.create(AppModule)
 
-  await app.listen(PORT);
+  await app.listen(PORT)
 }
-bootstrap();
+bootstrap()
