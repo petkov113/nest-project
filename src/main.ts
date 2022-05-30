dbf4a7d12156b03a2c843d7b0da5bb5564d59228
PP <pyotr.petkov@gmail.com>
2022-05-30 20:10:39 +0300

    add helmet

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
+import helmet from 'helmet'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const isProd = process.env.NODE_ENV === 'production'
   const PORT = process.env.PORT || 4000
   const app = await NestFactory.create(AppModule)
+
+  app.use(helmet())
 
   if (isProd) {
     const allowedOrigins = []
